b952da9b06a3f5167fdd893f3f4a12c1c8267076
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-09-27 22:02:07 +0300

    add features

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -24,8 +24,26 @@ export class Board {
         }
     }
 
+    public highlightCells(selectedCell: Cell | null) {
+        for (let i = 0; i < this.cells.length; i++) {
+            const row = this.cells[i]
+            for (let j = 0; j < row.length; j++) {
+                const target = row[j];
+                target.available = !!selectedCell?.figure?.canMove(target)
+                
+            }
+        }
+
+    }
+
     public getCell(x: number, y: number) {
         return this.cells[y][x]
+    }
+
+    getCopyBoard(): Board{
+        const newBoard = new Board();
+        newBoard.cells = this.cells;
+        return newBoard;
     }
 
     private addKings() {
@@ -68,6 +86,10 @@ export class Board {
 
     }
 
+
+/*    public addFisherFigures() {
+
+    }*/
 
     public addFigures() {
         this.addKings()
