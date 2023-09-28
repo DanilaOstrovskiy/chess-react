f9da8fdfc944d65efe2a98265e4ed99f1d7daa36
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-09-28 15:36:51 +0300

    Add new features

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -4,6 +4,7 @@ import {Cell} from "../Cell";
 import blackLogo from "../../assets/king-b.svg";
 import whiteLogo from "../../assets/king-w.svg";
 
+
 export class King extends Figure {
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
@@ -15,8 +16,12 @@ export class King extends Figure {
         const dy = Math.abs(target.y - this.cell.y);
 
         if ((dx === 1 && dy === 0) || (dx === 0 && dy === 1) || (dx === 1 && dy === 1)) {
-            return true;
+            // Check if the target cell is empty or occupied by an opponent's piece.
+            if (!target.isOccupied() || target.getOccupyingPiece()?.color !== this.color) {
+                return true;
+            }
         }
-        return false
+
+        return false;
     }
 }
--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -13,7 +13,7 @@ export class Rook extends Figure {
     }
     canMove(target: Cell): boolean {
         if (!super.canMove(target))
-            return false
+            return false;
         if (this.cell.isEmptyVertical(target))
             return true
         if (this.cell.isEmptyHorizontal(target))
