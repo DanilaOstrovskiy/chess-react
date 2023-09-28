4d6ed3c3b0b06fb58cff5571f177bd1265e2b7b4
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-09-28 12:47:46 +0300

    Added logic for the movement of the figures

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -8,11 +8,15 @@ export class King extends Figure {
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
-        this.name = FigureNames.BISHOP;
+        this.name = FigureNames.KING;
     }
     canMove(target: Cell): boolean {
-        if (!super.canMove(target))
-            return false
-        return true
+        const dx = Math.abs(target.x - this.cell.x);
+        const dy = Math.abs(target.y - this.cell.y);
+
+        if ((dx === 1 && dy === 0) || (dx === 0 && dy === 1) || (dx === 1 && dy === 1)) {
+            return true;
+        }
+        return false
     }
 }
--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -9,11 +9,15 @@ export class Rook extends Figure {
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
-        this.name = FigureNames.BISHOP;
+        this.name = FigureNames.ROOK;
     }
     canMove(target: Cell): boolean {
         if (!super.canMove(target))
             return false
-        return true
+        if (this.cell.isEmptyVertical(target))
+            return true
+        if (this.cell.isEmptyHorizontal(target))
+            return true
+        return false
     }
 }
